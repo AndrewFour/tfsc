7e84831789425a992c4b687d26b6e7e7c73898de
AndrewFour <65018915+AndrewFour@users.noreply.github.com>
2023-04-30 20:06:31 +0300

    Update tfsc_checking_run.sh

--- tfsc_checking_run.sh
+++ tfsc_checking_run.sh
@@ -1,19 +1,19 @@
-n=1
+unic42=1
 while true
 do
 
-if test $n -gt 720
+if test $unic42 -gt 720
 then
 tmux kill-session -t tfs_v0.22.1_fffda3e_devnet
 sleep 5
-n=0
+unic42=0
 printf "restart"
 printf "\n"
 fi
        tmux new-session -d -s tfsc 'cd $HOME/tfsc/ && $HOME/tfsc/tfs_v0.22.1_fffda3e_devnet -m'
-       n=$((n + 1))
+       unic42=$((unic42 + 1))
         printf "sleep " 
-        printf $n
+        printf $unic42
         for((sec=0; sec<10; sec++))
         do
                 printf "."
